d2e4d96b91e505e571db20750c3b90ed6f68cb86
Dylan Lynch <lynchd2@tcnj.edu>
2015-11-04 20:26:42 -0500

    create controller static_pages

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  get 'static_pages/about'
+
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  get 'static_pages/bout'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
