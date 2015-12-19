eb0015b865bc31033ed1a4d6b7d07944771c43fb
Dylan Lynch <lynchd2@tcnj.edu>
2015-12-19 09:44:03 -0500

    Add security function allowing users to only see their own events(as practice)

--- private-events/app/controllers/users_controller.rb
+++ private-events/app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:show]
+  before_action :correct_user, only: [:show]
+
   def new
   	@user = User.new
   end
@@ -24,6 +27,19 @@ class UsersController < ApplicationController
 
   def user_params
   	params.require(:user).permit(:name)
+  end
+
+
+  def logged_in_user
+     unless logged_in?
+      flash[:danger] = "Please log in."
+       redirect_to login_url
+     end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 
 end
