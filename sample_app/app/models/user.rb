7989cc018587a6e9a619470ec7bec9a9ec27fe65
Dylan Lynch <lynchd2@tcnj.edu>
2015-12-21 22:03:30 -0500

    Added model for followers/followed

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ActiveRecord::Base
 	attr_accessor :remember_token
-    has_many :microposts, dependent: :destroy
+  has_many :microposts, dependent: :destroy
+  has_many :active_relationships,  class_name:  "Relationship",
+                                   foreign_key: "follower_id",
+                                   dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
 	before_save {self.email = email.downcase}
 	validates :name, presence: true, length: {maximum: 50}
 	validates :email, presence: true, length: {maximum: 255}
@@ -37,4 +46,18 @@ class User < ActiveRecord::Base
     def feed
         Micropost.where("user_id = ?", id)
     end
-end
+
+
+      def follow(other_user)
+        active_relationships.create(followed_id: other_user.id)
+      end
+
+
+      def unfollow(other_user)
+        active_relationships.find_by(followed_id: other_user.id).destroy
+      end
+
+      def following?(other_user)
+        following.include?(other_user)
+      end
+    end
